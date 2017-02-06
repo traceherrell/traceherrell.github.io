49f27fc3bd4bbdb66bcf6628180d79fce8ff5b60
Trace Herrell <Trace Herrell>
2017-02-06 14:48:13 -0800

    adding vuejs

---
+++ src/components/Hello.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Vuejs</a></li>
+      <li><a href="https://webbluetoothcg.github.io/web-bluetooth/" target="_blank">Bluetooth Web Api</a></li>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">WebPack</a></li>
+    </ul>
+
+     <ul>
+      <li> <router-link to="/bluetooth">Bluetooth api playground</router-link></li>
+    
+    </ul>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome...  '
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
